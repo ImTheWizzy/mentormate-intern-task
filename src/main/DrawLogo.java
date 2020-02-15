70be2457a6274438083727364b41793d04c61aa6
ImTheWizzy <martin_todo9@abv.bg>
2020-02-16 01:49:31 +0200

    good progress

--- src/main/DrawLogo.java
+++ src/main/DrawLogo.java
@@ -1,10 +1,11 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DrawLogo {
 
-	public static boolean numberIsOdd(int numberToCheck) {
+	public static boolean isOdd(int numberToCheck) {
 		if(numberToCheck % 2 != 0) {
 			return true;
 		}
@@ -12,10 +13,59 @@ public class DrawLogo {
 	}
 	
 	public static boolean isValidLetterThickness(int letterThickness) {
-		if((letterThickness > 2 && letterThickness < 10000) && numberIsOdd(letterThickness)) {
+		if((letterThickness > 2 && letterThickness < 10000) && isOdd(letterThickness)) {
 			return true;
 		}
 		return false;
+	}
+	
+	static void printAsciiLogo(int letterThickness) {
+		String [][] asciiGrid = new String[letterThickness + 1][letterThickness * 10];
+		
+		int counter = letterThickness;
+		
+		boolean counterReachedZero = false;
+		
+		for(int columnCount = 0; columnCount < letterThickness * 10; columnCount++) { //FOR EACH CHUNK OF N SIZED CHARACTERS
+			for(int rowCount = 0; rowCount < letterThickness + 1; rowCount++) { //FOR VERTICAL LINES
+				if(counterReachedZero) {
+					if(rowCount < letterThickness - counter) {
+						asciiGrid[rowCount][columnCount] = "*";
+					} else {
+						asciiGrid[rowCount][columnCount] = "-";
+					}
+				} else {
+					if(rowCount >= counter) {
+						asciiGrid[rowCount][columnCount] = "*";
+					} else {
+						asciiGrid[rowCount][columnCount] = "-";
+					}	
+				}
+			}
+			
+			if(!counterReachedZero) {
+				counter--;
+				if(counter == 0) {
+					counterReachedZero = true;
+				}
+			} else if(counterReachedZero) {
+				counter++;
+				if(counter >= (letterThickness + 1) / 2) {
+					counterReachedZero = false;
+					counter--;
+				}
+//				if(counter == letterThickness) {
+//					counterReachedZero = false;
+//				}
+			}
+		}
+		
+		for(String[] x : asciiGrid) {
+			for(String y : x) {
+				System.out.print(y);
+			}
+			System.out.print('\n');
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -24,9 +74,7 @@ public class DrawLogo {
 		int letterThickness = inputStream.nextInt();
 		
 		if(isValidLetterThickness(letterThickness)) {
-			System.out.println("VALID!");
-		} else {
-			System.out.println("INVALID!");	
+			printAsciiLogo(letterThickness);
 		}
 		
 		inputStream.close();
--- src/test/DrawLogoTest.java
+++ src/test/DrawLogoTest.java
@@ -9,12 +9,12 @@ import main.DrawLogo;
 class DrawLogoTest {
 
 	@Test
-	void testNumberIsOdd() {		
-		assertEquals(true, DrawLogo.numberIsOdd(1));
-		assertEquals(false, DrawLogo.numberIsOdd(2));
-		assertEquals(true, DrawLogo.numberIsOdd(9999));
-		assertEquals(false, DrawLogo.numberIsOdd(10000));
-		assertEquals(true, DrawLogo.numberIsOdd(10001));
+	void testIsOdd() {		
+		assertEquals(true, DrawLogo.isOdd(1));
+		assertEquals(false, DrawLogo.isOdd(2));
+		assertEquals(true, DrawLogo.isOdd(9999));
+		assertEquals(false, DrawLogo.isOdd(10000));
+		assertEquals(true, DrawLogo.isOdd(10001));
 	}
 
 	@Test
