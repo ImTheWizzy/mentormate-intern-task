c3bda2ffddfbf4b65330f348fd60f927ef22746e
ImTheWizzy <martin_todo9@abv.bg>
2020-02-17 19:26:19 +0200

    cant be bothered to write a desc

--- src/main/DrawLogo.java
+++ src/main/DrawLogo.java
@@ -20,15 +20,32 @@ public class DrawLogo {
 	}
 	
 	static void printAsciiLogo(int letterThickness) {
-		String [][] asciiGrid = new String[letterThickness + 1][letterThickness * 10];
+		String [][] asciiGrid = new String[letterThickness + 1][letterThickness * 5];
 		
 		int counter = letterThickness;
 		
 		boolean counterReachedZero = false;
 		
-		for(int columnCount = 0; columnCount < letterThickness * 10; columnCount++) { //FOR EACH CHUNK OF N SIZED CHARACTERS
+		boolean midPointReached = false;
+		
+		for(int columnCount = 0; columnCount < letterThickness * 5; columnCount++) { //FOR EACH CHUNK OF N SIZED CHARACTERS
 			for(int rowCount = 0; rowCount < letterThickness + 1; rowCount++) { //FOR VERTICAL LINES
-				if(counterReachedZero) {
+//				if(!counterReachedZero && counter >= (letterThickness + 1) / 2) {
+//					if(rowCount < letterThickness - counter) {
+//						asciiGrid[rowCount][columnCount] = "*";
+//					} else {
+//						asciiGrid[rowCount][columnCount] = "-";
+//					}
+//					counter--;
+//				}
+//				else
+				if(midPointReached) {
+					if(rowCount <= counter) {
+						asciiGrid[rowCount][columnCount] = "*";
+					} else {
+						asciiGrid[rowCount][columnCount] = "-";
+					}
+				} else if(counterReachedZero) {
 					if(rowCount < letterThickness - counter) {
 						asciiGrid[rowCount][columnCount] = "*";
 					} else {
@@ -43,26 +60,29 @@ public class DrawLogo {
 				}
 			}
 			
-			if(!counterReachedZero) {
+			if(counter > 0 && !counterReachedZero) {
 				counter--;
-				if(counter == 0) {
+				if(counter <= 0) {
 					counterReachedZero = true;
 				}
-			} else if(counterReachedZero) {
+			} else {
 				counter++;
-				if(counter >= (letterThickness + 1) / 2) {
-					counterReachedZero = false;
-					counter--;
-				}
-//				if(counter == letterThickness) {
+//				if(counter >= (letterThickness + 1) / 2) {
 //					counterReachedZero = false;
 //				}
+				if(counter >= (letterThickness + 1) / 2) {
+					midPointReached = true;
+				}
+				if(counter >= letterThickness) {
+					counterReachedZero = false;
+					midPointReached = false;
+				}
 			}
 		}
 		
-		for(String[] x : asciiGrid) {
-			for(String y : x) {
-				System.out.print(y);
+		for(String[] rows : asciiGrid) {
+			for(String column : rows) {
+				System.out.print(column);
 			}
 			System.out.print('\n');
 		}
